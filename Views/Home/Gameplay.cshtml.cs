82027c441fe37873b0eed241b9678483990b296e
Mochacycle13 <mmmoore13@gmail.com>
2020-05-10 18:07:07 -0400

    small changes

--- Views/Home/Gameplay.cshtml.cs
+++ Views/Home/Gameplay.cshtml.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace End_of_all_Hope.Views.Home
-{
-    public class GameplayModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace End_of_all_Hope.Views.Home {
+    public class GameplayModel : PageModel {
+        private readonly ILogger<GameplayModel> _logger;
+
+        public GameplayModel (ILogger<GameplayModel> logger) {
+            _logger = logger;
         }
+        public void OnGet () { }
     }
 }
